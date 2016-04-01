6af8e53fc6605b719700c6c992b7bfa763f4903f
NormanGyllenhaal <me@yangpeng.tech>
2016-04-01 18:06:00 +0800

    Rejected commit: Default

--- src/main/java/site/lovecode/client/WechatThirdPartyClient.java
+++ src/main/java/site/lovecode/client/WechatThirdPartyClient.java
@@ -1,9 +1,13 @@
 package site.lovecode.client;
 
+import site.lovecode.support.bean.ComponentAccessTokenBean;
+
+import java.io.IOException;
+
 /**
  * Created by Administrator on 2016/4/1.
  */
 public interface WechatThirdPartyClient {
 
-
+    public ComponentAccessTokenBean getComponentAccessToken() throws IOException;
 }
