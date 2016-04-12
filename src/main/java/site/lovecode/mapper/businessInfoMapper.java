3ff451fa098fc2cd2f6e6889138d4c37a31207fd
NormanGyllenhaal <me@yangpeng.tech>
2016-04-12 18:09:31 +0800

    Rejected commit: Default

--- src/main/java/site/lovecode/mapper/businessInfoMapper.java
+++
@@ -1,7 +0,0 @@
-package site.lovecode.mapper;
-
-import site.lovecode.entity.businessInfo;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface businessInfoMapper extends Mapper<businessInfo> {
-}
